7d056fb8eb84b95653ab24a23758038d485aa0d6
studymachiney <1771134505@qq.com>
2023-08-28 11:20:04 +0800

    feat: 热门歌曲和热门歌手模块完成

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,3 +1,139 @@
-const TopPlay = () => <div>TopPlay</div>;
+/* eslint-disable import/no-unresolved */
+import { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper/modules';
+
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+const TopChartCard = ({
+  song,
+  i,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => (
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+    <h3 className="mr-3 text-base font-bold text-white">{i + 1}.</h3>
+    <div className="flex flex-row items-center justify-between flex-1">
+      <img
+        className="w-20 h-20 rounded-lg"
+        src={song?.images?.coverart}
+        alt={song?.title}
+      />
+      <div className="flex flex-col justify-center flex-1 mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="mt-1 text-base font-bold text-gray-300">
+            {song?.subtitle}
+          </p>
+        </Link>
+      </div>
+    </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={handlePlayClick}
+    />
+  </div>
+);
+
+const TopPlay = () => {
+  const dispath = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, []);
+
+  const topPlays = data?.slice(0, 5);
+
+  const handlePauseClick = () => {
+    dispath(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispath(setActiveSong({ song, data, i }));
+    dispath(playPause(true));
+  };
+
+  return (
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+    >
+      <div className="flex flex-col w-full">
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="font-bold text-white">排行榜</h2>
+          <Link to="/top-charts">
+            <p className="text-base text-gray-300 cursor-pointer">更多</p>
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-1 mt-4">
+          {topPlays?.map((song, i) => (
+            <TopChartCard
+              key={song.key}
+              song={song}
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full mt-8">
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="font-bold text-white">热门歌手</h2>
+          <Link to="/top-artists">
+            <p className="text-base text-gray-300 cursor-pointer">更多</p>
+          </Link>
+        </div>
+
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4"
+        >
+          {topPlays?.map((artist) => (
+            <SwiperSlide
+              key={artist.key}
+              style={{ width: '25%', height: 'auto' }}
+              className="rounded-full shadow-lg animate-slideright"
+            >
+              <Link to={`/artists/${artist?.artists[0].adamid}`}>
+                <img
+                  src={artist?.images.background}
+                  alt="name"
+                  className="object-cover w-full rounded-full"
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
