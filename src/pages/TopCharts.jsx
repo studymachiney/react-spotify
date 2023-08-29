27362f6b986252c52f68d8fdf7786498b2980679
studymachiney <1771134505@qq.com>
2023-08-29 16:00:31 +0800

    feat: 完成热门榜单页面

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,40 @@
-const TopCharts = () => <div>TopCharts</div>;
+import { useSelector } from 'react-redux';
+
+import { Error, Loader, SongCard } from '../components';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+const TopCharts = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) {
+    return <Loader title="正在加载热门榜单" />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="mt-4 mb-10 text-3xl font-bold text-left text-white">
+        发现热门榜单
+      </h2>
+
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TopCharts;
